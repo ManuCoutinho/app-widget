75a72be016f9d9e560c85561c74569fdb0c31e14
ManuCoutinho <gdn.sasha@gmail.com>
2022-05-03 22:19:37 -0300

    :sparkles: created POPOVER and BUTTONS

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,13 @@
+import { Popover } from '@headlessui/react'
+import { X } from 'phosphor-react'
+
+export const CloseButton: React.FC = () => {
+  return (
+    <Popover.Button
+      className='top-5 right absolute text-zinc-400 hover:text-zinc-100'
+      title='Fechar Formulário de feedback'
+    >
+      <X className='w-4 h-4' weight='bold' />
+    </Popover.Button>
+  )
+}
